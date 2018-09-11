b4413abcc84582dfab692f821111703ab6f9b51a
leessan0 <leessan0@cau.ac.kr>
2018-09-11 12:41:47 +0900

    180911_ProjectStart
    180911
    / MPAndroidChart_Realm
    / Aesthetic
    / AndroidMultiBluetoothLibrary

--- app/src/main/java/com/example/leessan0/lh_cau_ble/FullscreenActivity.java
+++ app/src/main/java/com/example/leessan0/lh_cau_ble/FullscreenActivity.java
@@ -19,6 +19,8 @@ public class FullscreenActivity extends AppCompatActivity {
      */
     private static final boolean AUTO_HIDE = true;
 
+    public String test;
+
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
